7c937370fafc94c14753988b9c5dfb5de09e02c1
Mariano Reyes <69815578+rey20074@users.noreply.github.com>
2021-03-10 22:21:34 -0600

    Graficas inciso A

--- proceso de pc.py
+++ proceso de pc.py
@@ -4,6 +4,7 @@ import random
 def proceso(proceso,env,timeprocess,CPU,RAM,cap,redIns,instruc):
     
     global Ttotal
+    global Promedio
     
     #se regresa la capacidad de la memoria
     yield RAM.get(cap)
@@ -25,7 +26,7 @@ def proceso(proceso,env,timeprocess,CPU,RAM,cap,redIns,instruc):
     print('El proceso %s ha finalizado con exito luego de haber empezado los procesos hace: %s segundos \n' % (proceso, env.now))
     RAM.put(cap)
     Ttotal = env.now
-
+    Promedio=timeprocess
 # ----------------------
 
 env = simpy.Environment() #ambiente de simulación
@@ -37,7 +38,7 @@ RAM = simpy.Container(env, init=100, capacity=100) #fijar la RAM
 Ttotal = 0
 
 for i in range(20):
-    env.process(proceso(i+1,env,random.randint(1,10),CPU,RAM,random.randint(20,40),3,random.randint(4,8)))
+    env.process(proceso(i+1,env,random.randint(1,10),CPU,RAM,random.randint(20,40),3,3))
 
 env.run(until=200)  #correr la simulación hasta el tiempo = 50
 
